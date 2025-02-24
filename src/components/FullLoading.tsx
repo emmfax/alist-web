855c7d256df3b52f10a9dc6686ea21fa1bf36dab
emmfax <faxlightgo@gmail.com>
2025-02-24 22:38:06 +0800

    2333333
    233333

---
+++ src/components/FullLoading.tsx
@@ -0,0 +1,71 @@
+import { Center, ElementType, Spinner, SpinnerProps } from "@hope-ui/solid"
+import { JSXElement, mergeProps, Show } from "solid-js"
+import { getMainColor } from "~/store"
+export const FullScreenLoading = () => {
+  return (
+    <Center h="100vh">
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        //主页圆形加载蓝色条
+        emptyColor="tomato"
+        //emptyColor="$neutral4"
+        //color="tomato"
+        color={getMainColor()}
+        size="xl"
+      />
+    </Center>
+  )
+}
+
+export const FullLoading = (props: {
+  py?: string
+  size?: string
+  thickness?: number
+  ref?: any
+}) => {
+  const merged = mergeProps(
+    {
+      py: "$8",
+      size: "xl",
+      thickness: 4,
+    },
+    props,
+  )
+  return (
+    <Center ref={props.ref} h="$full" w="$full" py={merged.py}>
+      <Spinner
+        thickness={`${merged.thickness}px`}
+        speed="0.65s"
+        //目录白色条
+        emptyColor="tomato"
+        //emptyColor="$neutral4"
+        //color="tomato"
+        color={getMainColor()}
+        size={merged.size as any}
+      />
+    </Center>
+  )
+}
+
+export const MaybeLoading = (props: {
+  children?: JSXElement
+  loading: boolean
+}) => {
+  return (
+    <Show when={!props.loading} fallback={<FullLoading />}>
+      {props.children}
+    </Show>
+  )
+}
+
+export const CenterLoading = <C extends ElementType = "div">(
+  props: SpinnerProps<C>,
+) => {
+  return (
+    <Center w="$full" h="$full">
+      {/* 左上角logo加载颜色 */}
+      <Spinner color="tomato" {...props} />
+    </Center>
+  )
+}
---
+++ src/pages/home/Obj.tsx
@@ -0,0 +1,107 @@
+import { Text, useColorModeValue, VStack } from "@hope-ui/solid"
+import {
+  createEffect,
+  createMemo,
+  createSignal,
+  lazy,
+  Match,
+  on,
+  Suspense,
+  Switch,
+} from "solid-js"
+import { Error, FullLoading, LinkWithBase } from "~/components"
+import { resetGlobalPage, useObjTitle, usePath, useRouter, useT } from "~/hooks"
+import {
+  getPagination,
+  objStore,
+  password,
+  recordHistory,
+  setPassword,
+  /*layout,*/ State,
+} from "~/store"
+
+const Folder = lazy(() => import("./folder/Folder"))
+const File = lazy(() => import("./file/File"))
+const Password = lazy(() => import("./Password"))
+// const ListSkeleton = lazy(() => import("./Folder/ListSkeleton"));
+// const GridSkeleton = lazy(() => import("./Folder/GridSkeleton"));
+
+const [objBoxRef, setObjBoxRef] = createSignal<HTMLDivElement>()
+export { objBoxRef }
+
+let first = true
+export const Obj = () => {
+  const t = useT()
+  const cardBg = useColorModeValue("white", "$neutral3")
+  const { pathname, searchParams } = useRouter()
+  const { handlePathChange, refresh } = usePath()
+  const pagination = getPagination()
+  const page = createMemo(() => {
+    return pagination.type === "pagination"
+      ? parseInt(searchParams["page"]) || 1
+      : undefined
+  })
+  let lastPathname = pathname()
+  let lastPage = page()
+  createEffect(
+    on([pathname, page], async ([pathname, page]) => {
+      useObjTitle()
+      if (!first) {
+        recordHistory(lastPathname, lastPage)
+        resetGlobalPage()
+      }
+      first = false
+      await handlePathChange(pathname, page)
+      lastPathname = pathname
+      lastPage = page
+    }),
+  )
+  return (
+    <VStack
+      ref={(el: HTMLDivElement) => setObjBoxRef(el)}
+      class="obj-box"
+      w="$full"
+      rounded="$xl"
+      bgColor={cardBg()}
+      p="$2"
+      shadow="$lg"
+      spacing="$2"
+    >
+      <Suspense fallback={<FullLoading />}>
+        <Switch>
+          <Match when={objStore.err}>
+            <Error msg={objStore.err} disableColor />
+          </Match>
+          <Match
+            when={[State.FetchingObj, State.FetchingObjs].includes(
+              objStore.state,
+            )}
+          >
+            <FullLoading />
+            {/* <Show when={layout() === "list"} fallback={<GridSkeleton />}>
+              <ListSkeleton />
+            </Show> */}
+          </Match>
+          <Match when={objStore.state === State.NeedPassword}>
+            <Password
+              title={t("home.input_password")}
+              password={password}
+              setPassword={setPassword}
+              enterCallback={() => refresh(true)}
+            >
+            <Text>{t("要不你猜一猜")}</Text>
+            </Password>
+          </Match>
+          <Match
+            when={[State.Folder, State.FetchingMore].includes(objStore.state)}
+          >
+            <Folder />
+          </Match>
+          <Match when={objStore.state === State.File}>
+            <File />
+          </Match>
+        </Switch>
+      </Suspense>
+    </VStack>
+  )
+}
